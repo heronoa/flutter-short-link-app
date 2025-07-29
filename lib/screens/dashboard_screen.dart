30ca49af54bac4bd2d3f48be7279485b99772bf5
Heron Amaral <heron.amaral@gmail.com>
2025-07-29 11:19:59 -0300

    feat: add link management features with create link form and link list

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import '../widgets/login_form.dart';
+import 'package:flutter_application_1/models/link.dart';
+import 'package:flutter_application_1/screens/create_link_screen.dart';
+import 'package:flutter_application_1/widgets/link_list.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
+
+  void onAddButtonPressed(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const CreateLinkScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +23,28 @@ class DashboardScreen extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Text(
-            'Welcome to the Dashboard!',
-            style: Theme.of(context).textTheme.headlineMedium,
+          child: LinkList(
+            links: [
+              Link(
+                url: 'url',
+                shortenedUrl: 'shortenedUrl',
+                title: 'title',
+                description: 'description',
+              ),
+            ],
+            onLinkTap: (link) {
+              final linkMap = link as Map<String, String>;
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Tapped on ${linkMap['title']}')),
+              );
+            },
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => onAddButtonPressed(context),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.add),
       ),
     );
   }
